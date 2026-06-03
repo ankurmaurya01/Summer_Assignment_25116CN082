e013f3871b7ecd5680436faeaa73bca5bda897a4
ankurmaurya01 <ankur.maurya626@gmail.com>
2026-06-03 17:04:58 +0530

    Day-3 assignment is uploaded

---
+++ Day-3/Q10.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+//program to Print prime numbers in a range
+int main() {
+    int s, e;
+    cout << "Enter the starting number: ";
+    cin >> s;
+    cout << "Enter the ending number: ";
+    cin >> e;
+
+    cout << "Prime numbers between " << s<< " and " << e << " are: ";
+
+    for (int n = s; n <= e; n++) {
+        if (n <= 1) continue;   // skip numbers less than or equal to 1
+        bool isPrime = true;
+
+        // check divisibility
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if (isPrime)
+            cout << n << " ";
+    }
+
+    cout << endl;
+    return 0;
+}
