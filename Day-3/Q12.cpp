3db90b3d6c1797ab6ca47002d71ff28bed6e237a
Ankur Maurya <156187708+ankurmaurya01@users.noreply.github.com>
2026-06-03 17:12:50 +0530

    Implement LCM calculation for two numbers
    This program calculates the Least Common Multiple (LCM) of two user-input numbers using the Greatest Common Divisor (GCD).

---
+++ Day-3/Q12.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+//program to Find LCM of two numbers
+int main() {
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    int lcm;
+    int gcd = 1;
+    for (int i = 1; i <= a && i <= b; i++) {
+        if (a % i == 0 && b % i == 0) {
+            gcd = i;
+        }
+    }
+    lcm = (a * b) / gcd;
+    cout << "LCM of " << a << " and " << b << " is: " << lcm << endl;
+    return 0;
+}
