bdaf68c924a28a6033abc8545962a5a80c0be788
Ankur Maurya <156187708+ankurmaurya01@users.noreply.github.com>
2026-06-05 12:33:29 +0530

    Simplify prime number check in main
    Refactor prime checking logic into main function.

--- Day-3/Q9.cpp
+++ Day-3/Q9.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
 using namespace std;
-
-int isPrime(int n){
-    if(n<=1) return 0;
-    for(int i=2;i<n;i++) if(n % i == 0) return 0;
-    return 1;
-}
-
-int main(){
+// Program to check if a number is prime
+int main() {
     int n;
-    cout<<"Enter a number: ";
-    if(!(cin>>n)) return 0;
-    if(isPrime(n)) cout<<n<<" is prime"<<endl;
-    else cout<<n<<" is not prime"<<endl;
+    cout << "Enter a number: ";
+    cin >> n;
+    int isPrime = 1;
+    for (int i = 2; i < n; i++) {
+        if (n % i == 0) {
+            isPrime = 0;
+            break;
+        }
+    }
+    if (isPrime == 1) {
+        cout << n << " is prime" << endl;
+    } else {
+        cout << n << " is not prime" << endl;
+    }
+
     return 0;
 }
